55b4059274cf9db7f48c4aa5c01cab35f8a6882f
Crankygirl <33200967+Crankygirl@users.noreply.github.com>
2017-10-29 22:30:20 +0530

    Update ExeploreGame.java

--- src/exeploreGame/ExeploreGame.java
+++ src/exeploreGame/ExeploreGame.java
@@ -6,12 +6,12 @@ public class ExeploreGame {
     final static String userId;
 
     public static void main(String[] args) {
-        while(true){
+        while (true) {
             final String password = JOptionPane.showInputDialog(null, "Enter Password", "Password", JOptionPane.INFORMATION_MESSAGE);
-                if(password ==null)continue;
+                if(password == null) continue;
                 if(password.compareTo("cheema")==0)break;
         }
-        while(true){
+        while (true) {
                 final userId =JOptionPane.showInputDialog(null,"Enter User ID","user id",JOptionPane.INFORMATION_MESSAGE);
                 if(userId==null)continue;
                 if(userId.compareTo("")!=0)break;
